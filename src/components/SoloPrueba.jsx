390ddc244ce278c4dfd440f394983d30e90b84e7
samueltaub@yahoo.com <samueltaub@yahoo.com>
2024-05-02 14:10:24 -0400

    agregue quite llamda a component v2

--- src/components/SoloPrueba.jsx
+++ src/components/SoloPrueba.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SoloPrueba() {
     return (
         <>
-            <h1 className='w-full mx-auto text-center'>SoloPrueba</h1>
+            <h1>SoloPrueba</h1>
             {/* <button onClick={() => alert("dale pué")} className='w-fit p-4 bg-red-400 flex flex-col gap-1justify-center items-center mx-auto rounded-lg'>
 
                 <p > ClickMe !</p>
