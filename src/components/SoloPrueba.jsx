6367a1bdeb93b4652d24a1f74f2e8af68bdb3879
samueltaub@yahoo.com <samueltaub@yahoo.com>
2024-05-02 13:55:37 -0400

    agregue n comnente de React con client:load V2

--- src/components/SoloPrueba.jsx
+++ src/components/SoloPrueba.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 function SoloPrueba() {
-    return (<div onClick={() => alert("dale pué")} className='w-fit p-4 bg-slate-400 flex flex-col gap-1justify-center items-center m-auto rounded-lg'>
-        <h1 className='w-full'>SoloPrueba</h1>
-        <button > ClickMe !</button>
-    </div>
+    return (
+        <>
+            <h1 className='w-full mx-auto text-center'>SoloPrueba</h1>
+            <button onClick={() => alert("dale pué")} className='w-fit p-4 bg-red-400 flex flex-col gap-1justify-center items-center mx-auto rounded-lg'>
+
+                <p > ClickMe !</p>
+            </button>
+        </>
     )
 }
 
