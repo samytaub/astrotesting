504131b6a6fd45ee3227f2a926b57a9278989fde
samueltaub@yahoo.com <samueltaub@yahoo.com>
2024-05-02 14:06:35 -0400

    agregue quite llamda a component

--- src/components/SoloPrueba.jsx
+++ src/components/SoloPrueba.jsx
@@ -4,10 +4,10 @@ function SoloPrueba() {
     return (
         <>
             <h1 className='w-full mx-auto text-center'>SoloPrueba</h1>
-            <button onClick={() => alert("dale pué")} className='w-fit p-4 bg-red-400 flex flex-col gap-1justify-center items-center mx-auto rounded-lg'>
+            {/* <button onClick={() => alert("dale pué")} className='w-fit p-4 bg-red-400 flex flex-col gap-1justify-center items-center mx-auto rounded-lg'>
 
                 <p > ClickMe !</p>
-            </button>
+            </button> */}
         </>
     )
 }
