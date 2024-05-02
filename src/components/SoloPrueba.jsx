d3fe820d50a9b4bb63000732491a559f1f84889e
samueltaub@yahoo.com <samueltaub@yahoo.com>
2024-05-02 13:35:44 -0400

    agregue n comnente de React con client:load

---
+++ src/components/SoloPrueba.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SoloPrueba() {
+    return (<div onClick={() => alert("dale pué")} className='w-fit p-4 bg-slate-400 flex flex-col gap-1justify-center items-center m-auto rounded-lg'>
+        <h1 className='w-full'>SoloPrueba</h1>
+        <button > ClickMe !</button>
+    </div>
+    )
+}
+
+export default SoloPrueba
