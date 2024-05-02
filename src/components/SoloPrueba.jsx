5da022ebe4b4607ae70b1d0dfe2db6a211555ff1
samueltaub@yahoo.com <samueltaub@yahoo.com>
2024-05-02 14:52:16 -0400

    agregue vercel adapter with react component hydrated

--- src/components/SoloPrueba.jsx
+++ src/components/SoloPrueba.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function SoloPrueba() {
     return (
         <>
-            <h1>SoloPrueba</h1>
-            {/* <button onClick={() => alert("dale pué")} className='w-fit p-4 bg-red-400 flex flex-col gap-1justify-center items-center mx-auto rounded-lg'>
+            <h1 className='w-full mx-auto text-center'>SoloPrueba</h1>
+            <button onClick={() => alert("dale pué")} className='w-fit p-4 bg-red-400 flex flex-col gap-1justify-center items-center mx-auto rounded-lg'>
 
                 <p > ClickMe !</p>
-            </button> */}
+            </button>
         </>
     )
 }
